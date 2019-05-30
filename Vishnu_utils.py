f62c85d1bd0125358af8e4892e757de1e31835b6
Vishnu Prakash Singh <30570477+Vishnu1183@users.noreply.github.com>
2019-05-30 11:23:42 +0530

    Vishnu_utils

---
+++ Vishnu_utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# getting feature importance of variables with name 
+def feature_importance(model):
+    return(pd.DataFrame({'features' : model.get_booster().feature_names, 'importance' : model.feature_importances_}).sort_values('importance',ascending = False))
+
+feat_imp = feature_importance(model = model_name)
