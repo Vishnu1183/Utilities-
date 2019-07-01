48559750971d60f17c908b2c94ab55c9ef9b0fd7
Vishnu Prakash Singh <30570477+Vishnu1183@users.noreply.github.com>
2019-07-01 17:05:37 +0530

    added jaccard_distance function
    jaccard_distance  calculates jaccard distance between two strings

--- Vishnu_utils.py
+++ Vishnu_utils.py
@@ -1,8 +1,38 @@
 import pandas as pd
 import numpy as np
 
-# getting feature importance of variables with name for XGB model
+# 1- getting feature importance of variables with name for XGB model
 def feature_importance(model):
     return(pd.DataFrame({'features' : model.get_booster().feature_names, 'importance' : model.feature_importances_}).sort_values('importance',ascending = False))
 
 feat_imp = feature_importance(model = model_name)
+
+
+
+# 2- getting jaccard distance from 2 strings 
+def jaccard_distance(a,b,q=2):
+    """
+    Equivalent of jaccard distance from stringdist in R.
+    Calculates jaccard distance with ngram = 2 for 2 strings
+    
+    a , b the strings for which jaccard distance is to be calculcated
+    q = n of n-gram
+    """
+    if a == b:
+        return 1
+    
+    ngram_a = []
+    for i in range(len(a)-(q-1)):
+        temp_a= a[i:i+q]
+        ngram_a.append(temp_a)
+        
+    ngram_b = []
+    for j in range(len(b)-(q-1)):
+        temp_b= b[j:j+q]
+        ngram_b.append(temp_b)
+        
+    set_a= set(ngram_a)
+    set_b= set(ngram_b)
+    #intersection = set_a.intersection(set_b)
+    #union = set_b.union(set_a)
+    return 1- (len(set_a.intersection(set_b)) /len(set_a.union(set_b)))
