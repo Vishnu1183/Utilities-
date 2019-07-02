2e15a12d2fc69aba777a5edb1b7773a6b4d70549
Vishnu Prakash Singh <30570477+Vishnu1183@users.noreply.github.com>
2019-07-02 09:25:47 +0530

    changed order of  ngram and jaccard

--- Vishnu_utils.py
+++ Vishnu_utils.py
@@ -8,13 +8,27 @@ def feature_importance(model):
 feat_imp = feature_importance(model = model_name)
 
 
-# 2- getting jaccard distance from 2 strings 
+
+# 2- getting ngrams from string
+def ngrams(s,n):
+    """
+    generates the ngrams of a string.
+    """
+    ngram_s = []
+    for i in range(len(s)-(n-1)):
+        temp_s= s[i:i+n]
+        ngram_s.append(temp_s)
+    return ngram_s
+
+
+
+# 3- getting jaccard distance from 2 strings 
 def jaccard_distance(a,b,q=2):
     """
     Equivalent of jaccard distance from stringdist in R.
-    Calculates jaccard distance with ngram = 2 for 2 strings  
-    a , b the strings for which jaccard distance is to be calculcated
-    q = n of n-gram
+    Calculates jaccard distance with ngram = 2 for 2 strings.  
+    a , b the strings for which jaccard distance is to be calculcated.
+    q = n of n-gram.
     """
     if a == b:
         return 1 
@@ -30,15 +44,4 @@ def jaccard_distance(a,b,q=2):
     #intersection = set_a.intersection(set_b)
     #union = set_b.union(set_a)
     return 1- (len(set_a.intersection(set_b)) /len(set_a.union(set_b)))
-
-# 3- getting ngrams from string
-def ngrams(s,n):
-    """
-    generates the ngrams of a string
-    """
-    ngram_s = []
-    for i in range(len(s)-(n-1)):
-        temp_s= s[i:i+n]
-        ngram_s.append(temp_s)
-    return ngram_s
 
