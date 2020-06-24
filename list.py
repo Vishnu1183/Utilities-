849e7c4095a33efb6a57ea0e016d81218b73b66c
Vishnu Prakash Singh <30570477+Vishnu1183@users.noreply.github.com>
2020-06-24 11:32:34 +0530

    Update list.py

--- list.py
+++ list.py
@@ -17,3 +17,17 @@ nbr_loop = int(max(np.where(np.isnan(np.divide(mdn,dmn)),0,np.ceil(np.divide(mdn
 for i in range(nbr_loop):
     print(f'remaining demand after {i+1} runs')
     print(np.where(mdn-(i+1)*dmn < 0,0,mdn-(i+1)*dmn), '\n')
+    
+    
+    
+dd = sum(dmn)
+for i in range(nbr_loop):
+    rem_aftr_nruns = np.where(mdn-(i+1)*dmn < 0,0,mdn-(i+1)*dmn)
+    len(np.array(np.where(rem_aftr_nruns==0)).ravel())
+    if len(np.array(np.where(rem_aftr_nruns==0)).ravel()) == 0:
+        rem_aftr_nruns1 = rem_aftr_nruns
+    else:
+        rem_aftr_nruns1 = (1+sum(dmn[np.where(rem_aftr_nruns==0)])/dd)*rem_aftr_nruns
+    
+    print(f'remaining demand after {i+1} runs')
+    print(rem_aftr_nruns1, '\n')
